713fa7c2888d86d7e771fa40625db9bdf0214e4e
Abner Borgonha <abnerborgonha@gmail.com>
2021-09-23 23:30:27 -0300

    feat: provide the character and comic routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import usersRouter from '@modules/users/infra/http/routes/users.route'
+import comicsRouter from '@modules/comics/infra/http/routes/comics.route'
 import charactersRouter from '@modules/characters/infra/http/routes/characters.route'
 import sessionRouter from '@modules/users/infra/http/routes/session.route'
 
@@ -8,6 +9,7 @@ const routes = Router()
 
 routes.use('/session', sessionRouter)
 routes.use('/users', usersRouter)
+routes.use('/favorite-comics', comicsRouter)
 routes.use('/favorite-characters', charactersRouter)
 
 export default routes
